7d65dd1a9d88e0d39a901f375080f75258824f1d
doitcomet <148560062+doitcomet@users.noreply.github.com>
2026-08-16 21:45:18 +0900

    Fix GitHub Pages routing with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
 
@@ -31,9 +31,9 @@ class AppErrorBoundary extends React.Component {
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppErrorBoundary>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </AppErrorBoundary>
   </React.StrictMode>
 );
